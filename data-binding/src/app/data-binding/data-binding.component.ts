4fc7278c0c2c939d367835cdc6aa774f2d007a52
fagnerdev.souza <fagner.cicero@gmail.com>
2020-06-16 21:08:45 -0300

    two-way

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,12 +10,27 @@ export class DataBindingComponent implements OnInit {
   url: string = 'http://loiane.com';
   urlImage = 'http://lorempixel.com/400/200/nature/';
   cursoAngular = true;
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
 
   getValor(){
     return 1;
   }
   getCurtirCurso(){
-    return true;
+    return true; 
+  }
+  botaoClicado(){
+    alert("Botão Clicado!")
+  }
+  onKeyUP(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
   constructor() { }
 
