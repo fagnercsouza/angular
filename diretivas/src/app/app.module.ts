7a8137c9e142e60da5e9ac0c1169332128a77c0e
fagnerdev.souza <fagner.cicero@gmail.com>
2020-06-26 14:11:37 -0300

    diretivas

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 // import { BsDropdownModule} from 'ngx-bootstrap/dropdown';
 // import { TooltipModule} from 'ngx-bootstrap/tooltip';
@@ -10,16 +11,21 @@ import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
+import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
+import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
     DiretivaNgswitchComponent,
-    DiretivaNgforComponent
+    DiretivaNgforComponent,
+    DiretivaNgclassComponent,
+    DiretivaNgstyleComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
     // BsDropdownModule.forRoot(),
     // TooltipModule.forRoot(),
