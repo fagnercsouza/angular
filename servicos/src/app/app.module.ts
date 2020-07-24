c17c0f1442e627f1d8e9a718e505f8d413e024c0
fagnercsouza <fagner.cicero@gmail.com>
2020-07-24 16:46:08 -0300

    acesso servico

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -3,23 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
+// import { CursosComponent } from './cursos/cursos.component';
 import { CursosService} from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
-import { ReceberCursoCriadoComponent } from './receber-curso-criado/receber-curso-criado.component';
+import { CursosModule } from './cursos/cursos.module';
+// import { ReceberCursoCriadoComponent } from './receber-curso-criado/receber-curso-criado.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CursosComponent,
-    ReceberCursoCriadoComponent
+    AppComponent//,
+    // CursosComponent//,
+    // ReceberCursoCriadoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CriarCursoModule
+    CriarCursoModule,
+    CursosModule
   ],
-  providers: [CursosService],
+  // providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
