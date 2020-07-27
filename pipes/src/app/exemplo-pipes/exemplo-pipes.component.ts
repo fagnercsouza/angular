da3176d67c42dd39c312a1fc09cb8edb4db7345b
fagnerdev.souza <fagner.cicero@gmail.com>
2020-07-27 10:42:11 -0300

    exemplo-pipes

---
+++ pipes/src/app/exemplo-pipes/exemplo-pipes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplo-pipes',
+  templateUrl: './exemplo-pipes.component.html',
+  styleUrls: ['./exemplo-pipes.component.css']
+})
+export class ExemploPipesComponent implements OnInit {
+
+  livro: any = {
+    titulo: 'Aprendendo Javascript com estrutura de dados',
+    rating: 4.54321,
+    numeroPaginas: 341,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23),
+    url: 'http://a.co/glqjpRP'
+   }
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
