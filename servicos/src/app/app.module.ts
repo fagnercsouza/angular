9a8c5c8ff14e828350090b0d257ab1dcf96f0f3f
fagnercsouza <fagner.cicero@gmail.com>
2020-07-27 17:26:03 -0300

    pipes

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -4,15 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // import { CursosComponent } from './cursos/cursos.component';
-import { CursosService} from './cursos/cursos.service';
+// import { CursosService} from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
-<<<<<<< HEAD
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    CursosComponent
-=======
 import { CursosModule } from './cursos/cursos.module';
 // import { ReceberCursoCriadoComponent } from './receber-curso-criado/receber-curso-criado.component';
 
@@ -21,7 +14,6 @@ import { CursosModule } from './cursos/cursos.module';
     AppComponent//,
     // CursosComponent//,
     // ReceberCursoCriadoComponent
->>>>>>> c17c0f1442e627f1d8e9a718e505f8d413e024c0
   ],
   imports: [
     BrowserModule,
